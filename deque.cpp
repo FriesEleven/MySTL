2c21edfedc49b9750d221089891c24fd468e9a9f
Fries <suoheyi11@163.com>
2024-10-09 17:26:07 +0800

    deque in stl

--- deque.cpp
+++ deque.cpp
@@ -40,12 +40,73 @@ public:
         if(size == capacity) {
             resize();
         }
-        frontIndex = (frontIndex - 1 + capacity);
+        frontIndex = (frontIndex - 1 + capacity) % capacity;
         elements[frontIndex] = value;
         ++size;
     }
 
     void push_back(const T& value) {
-        
+        if(size == capacity) {
+            resize();
+        }
+        elements[backIndex] = value;
+        backIndex = (backIndex + 1) % capacity;
+        ++size;
+    }
+
+    void pop_front() {
+        if(size == 0) {
+            throw std::out_of_range("Deque is empty");
+        }
+        frontIndex = (frontIndex + 1) % capacity;
+        --size;
+    }
+
+    void pop_back() {
+        if(size == 0) {
+            throw std::out_of_range("Deque is empty");
+        }
+        backIndex = (backIndex -  1 + capacity) % capacity;
+        --size;
+    }
+
+    T& operator[](int index) {
+        if(index < 0 || index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        return elements[(frontIndex + index) % capacity];
+    }
+
+    size_t getSize() {
+        return size;
+    }
+
+    void clear() {
+        while(size > 0) {
+            pop_front();
+        }
+    }
+
+    void printElements() const {
+        size_t index = frontIndex;
+        for(size_t i = 0; i < size; ++i) {
+            std::cout << elements[index] << " ";
+            index = (index + 1) % capacity;
+        }
+        std::cout << std::endl;
     }
 };
+
+int main() {
+    Deque<int> mydeque;
+    mydeque.push_back(0);
+    mydeque.push_front(1);
+    auto x = mydeque[0];
+    std::cout << x <<std::endl;
+    mydeque.printElements();
+    mydeque.pop_back();
+    mydeque.printElements();
+    mydeque.pop_front();
+    mydeque.clear();
+    return 0;
+}
