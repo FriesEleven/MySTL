67da010a2539a020128e0abfdbd18a28182e6fc5
Fries <suoheyi11@163.com>
2024-10-16 16:49:47 +0800

    red_black_tree(10.16)

--- red_black_tree.cpp
+++ red_black_tree.cpp
@@ -76,6 +76,114 @@ private:
     }
 
     void insertFixup(Node *target) {
+       while(target->parent && target->parent->color == Color::RED) {
+        if(target->parent == target->parent->parent->left) {
+            Node *uncle = target->parent->parent->right;
+            if(uncle && uncle->color == Color::RED) {
+                target->parent->color = Color::BLACK;
+                uncle->color = Color::BLACK;
+                target->parent->parent->color = Color::RED;
+                target = target->parent->parent;
+            } else {
+                if(target && target == target->parent->right) {
+                    target = target->parent;
+                    rightRotate(target);
+                }
+                target->parent->color == Color::BLACK;
+                target->parent->parent->color = Color::RED;
+                rightRotate(target->parent->parent);
+            }
+        } else {
+            Node *uncle = target->parent->parent->left;
+            if(uncle && uncle->color == Color::RED) {
+                target->parent->color = Color::BLACK;
+                uncle->color = Color::BLACK;
+                target->parent->parent->color = Color::RED;
+                target = target->parent->parent;
+            } else {
+                if(target && target == target->parent->left) {
+                    target = target->parent;
+                    rightRotate(target);
+                }
+                target->parent->color = Color::BLACK;
+                target->parent->parent->color = Color::RED;
+                leftRotate(target->parent->parent);
+            }
+        }
+       }
+       root->color = Color::BLACK;
+    }
+
+    void insertNode(cosnt Key &key, const Value &value) {
+        Node *newNode = new Node(key, value, Color::RED);
+        Node *parent = nullptr;
+        Node *cmpNode = root;
+        while(cmpNode) {
+            parent = cmpNode;
+            if(newNode->key < cmpNode->key) {
+                cmpNode = cmpNode->left;
+            } else if(newNode->key > cmpNode->key) {
+                cmpNode = cmpNode->right;
+            } else {
+                delete newNode;
+                return;
+            }
+        }
+        size++;
+        newNode->parent = parent;
+        if(!parent) {
+            root = newNode;
+        } else if(newNode->key < parent->key) {
+            parent->left = newNode;
+        } else {
+            parent->right = newNode;
+        }
+        insertFixup(newNode);
+    }
+
+    void inorderTraversal(Node *node) const {
+        if(node) {
+            inorderTraversal(node->left);
+            std::cout << node->key << " ";
+            std::cout << node->value << "";
+            inorderTraversal(node->right);
+        }
+    }
+
+    void replaceNode(Node *target, Node *newNode) {
+        if(!target->parent) {
+            root = target;
+        } else if(target = target->parent->left) {
+            target->parent->left = newNode;
+        } else {
+            target->parent->right = newNode;
+        }
+        if(newNode) {
+            newNode->parent = target->parent;
+        }
+    }
+
+    Node *findMinmunNode(Node *node) {
+        while(node->left) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    Color getColor(Node *node) {
+        if(node == nullptr) {
+            return Color::BLACK;
+        }
+        return node->color;
+    }
+    
+    void setColor(Node *node, Color color) {
+        if(node == nullptr) {
+            return;
+        }
+        node->color = color;
+    }
+    void removeFixup(Node *node) {
         
     }
 };
